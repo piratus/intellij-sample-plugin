9b396fce106f6e2badf867060dee5d3cf1e02b42
GitHub Action <action@github.com>
2021-09-21 17:32:45 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/piratus/intellijsampleplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.piratus.intellijsampleplugin.services
+
+import com.github.piratus.intellijsampleplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
